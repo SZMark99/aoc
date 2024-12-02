d60be492c7b28e281fa8f94154af494da82c525f
szabo <szabomark9900@gmail.com>
2024-12-03 00:32:05 +0100

    day2 second

--- 2024/src/day2/SafeNumberRow.java
+++ 2024/src/day2/SafeNumberRow.java
@@ -1,5 +1,6 @@
 package day2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,25 +11,30 @@ public class SafeNumberRow {
         for (String line : text.split("\n")) {
             List<String> parts = Arrays.asList(line.split(" "));
 
-            if (isSafe(parts.stream().map(Integer::parseInt).collect(Collectors.toList()))) {
+            if (isSafe(parts.stream().map(Integer::parseInt).collect(Collectors.toList()), false)) {
                 safe++;
             }
         }
         System.out.println(safe);
     }
 
-    private boolean isSafe(List<Integer> parts) {
+    private boolean isSafe(List<Integer> parts, boolean remove) {
         boolean decrease = false;
         boolean increase = false;
         for (int i = 1; i < parts.size(); i++) {
             int sub = Math.abs(parts.get(i) - parts.get(i - 1));
-            if (sub > 3 || sub == 0) {
-                return false;
-            }
-            if (decrease && parts.get(i) > parts.get(i - 1)) {
-                return false;
-            }
-            if (increase && parts.get(i) < parts.get(i - 1)) {
+            if (sub > 3 || sub == 0 ||
+                    decrease && parts.get(i) > parts.get(i - 1) ||
+                    increase && parts.get(i) < parts.get(i - 1)) {
+                if (!remove) {
+                    for (int j = 0; j < parts.size(); j++) {
+                        List<Integer> copy = new ArrayList<>(parts);
+                        copy.remove(j);
+                        if (isSafe(copy, true)) {
+                            return true;
+                        }
+                    }
+                }
                 return false;
             }
             if (parts.get(i) < parts.get(i - 1)) {
