04f5e171dff9fa86d5291f55d043733e019fab84
szabo <szabomark9900@gmail.com>
2024-12-02 22:59:16 +0100

    day1

---
+++ 2024/src/main/java/day1/DistanceSum.java
@@ -0,0 +1,29 @@
+package day1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DistanceSum {
+    private ListsDistence listsDistence = new ListsDistence();
+    private void sum(List<Integer> distances) {
+        int sum = 0;
+        for (int distance : distances) {
+            sum += distance;
+        }
+        System.out.println("Sum: " + sum);
+    }
+
+    private void reader(String text) {
+        List<Integer> leftList = new ArrayList<>();
+        List<Integer> rightList = new ArrayList<>();
+        for (String row : text.split("\n")) {
+            String[] strings = row.split("   ");
+            leftList.add(Integer.parseInt(strings[0]));
+            rightList.add(Integer.parseInt(strings[1]));
+        }
+        leftList.sort(Integer::compareTo);
+        rightList.sort(Integer::compareTo);
+        sum(listsDistence.getDistances(leftList, rightList));
+    }
+
+}
