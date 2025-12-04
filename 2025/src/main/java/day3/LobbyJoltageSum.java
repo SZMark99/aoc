e730110f214cfe50eca6fd0e5603e0b33d1ee8ec
Szabó István Márk <szabo.mark@webstar.hu>
2025-12-04 01:19:05 +0100

    2025 day 3 part 2

--- 2025/src/main/java/day3/LobbyJoltageSum.java
+++ 2025/src/main/java/day3/LobbyJoltageSum.java
@@ -1,19 +1,40 @@
 package day3;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class LobbyJoltageSum {
+    static int maxIndex = 0;
+
     public static void read(String text) {
-        long sum = Arrays.stream(text.split("\n"))
-                .mapToLong(LobbyJoltageSum::getMaxJoltage)
+        double sum = Arrays.stream(text.split("\n"))
+                .mapToDouble(LobbyJoltageSum::getMaxJoltage)
                 .sum();
-        System.out.println("Joltage Sum: " + sum);
+        System.out.println("Joltage Sum: " + (long) sum);
     }
 
-    private static int getMaxJoltage(String line) {
-        int max = Arrays.stream(line.substring(0, line.length() - 1).split("")).mapToInt(Integer::parseInt).max().orElse(0);
-        int maxIndex = line.indexOf((char) (max + '0'));
-        int max2 = Arrays.stream(line.substring(maxIndex + 1).split("")).mapToInt(Integer::parseInt).max().orElse(0);
-        return max * 10 + max2;
+    private static double getMaxJoltage(String line) {
+        maxIndex = 0;
+        double sum = IntStream.rangeClosed(1, 12)
+                .mapToDouble(i -> {
+                    int max = getMax(line, i);
+                    String substring = line.substring(maxIndex + plusOne(i));
+                    maxIndex += substring.indexOf((char) (max + '0')) + plusOne(i);
+                    return max * Math.pow(10, 12 - i);
+                })
+                .sum();
+        System.out.println("maxJoltage: " + (long) sum);
+        return sum;
+    }
+
+    private static int plusOne(int i) {
+        return i == 1 ? 0 : 1;
+    }
+
+    private static int getMax(String line, int i) {
+        return Arrays.stream(line.substring(maxIndex + plusOne(i), line.length() - 12 + i)
+                        .split(""))
+                .mapToInt(Integer::parseInt)
+                .max().orElse(0);
     }
 }
